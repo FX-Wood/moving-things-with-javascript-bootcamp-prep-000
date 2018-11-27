0f51a4856ebf815f1ea4ce42c4754ed954e1dd31
FX Wood <fxwood2@gmail.com>
2018-11-27 03:29:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ function keyHandler(e) {
   if (e.key === "ArrowLeft") {
       var leftNumbers = dodger.style.left.replace('px','')
       var left = parseInt(leftNumbers, 10)
+      dodger.style.left = `${left - 1}px`
 
-      dodger.style.left = `${left -1}px`
   } else if (e.key === "ArrowRight") {
     var leftNumbers = dodger.style.left.replace('px','')
     var left = parseInt(leftNumbers, 10)
+    dodger.style.left = `${left + 1}px`
 
-    dodger.style.left = `${left +1}px`
+  } else if (e.key === "ArrowUp") {
+    var bottomNumbers = dodger.style.bottom.replace('px','')
+    var bottom = parseInt(bottomNumbers, 10)
+    dodger.style.bottom = `${bottom + 1}px`
   }
 }
 
