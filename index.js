ecb6c1e793c5ffa51226ca977c0001df3cc3733d
FX Wood <fxwood2@gmail.com>
2018-11-27 03:25:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much."
+
+function keyHandler(e) {
+  if (e.key === "ArrowLeft") {
+      var leftNumbers = dodger.style.left.replace('px','')
+      var left = parseInt(leftNumbers, 10)
+
+      dodger.style.left = `${left -1}px`
+  } else if (e.key === "ArrowRight") {
+    var leftNumbers = dodger.style.left.replace('px','')
+    var left = parseInt(leftNumbers, 10)
+
+    dodger.style.left = `${left +1}px`
+  }
+
+
+}
+
+
+documemt.addEventListener('keydown', keyHandler)
